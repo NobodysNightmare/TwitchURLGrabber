9ee1edb4193fcc4dacd9559916f4835fc837b772
Jan Sandbrink <fischikowski@web.de>
2013-04-30 20:10:21 +0200

    enable updates of total count

--- TwitchURLGrabber/URLListItem.cs
+++ TwitchURLGrabber/URLListItem.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 
 namespace TwitchURLGrabber
 {
-    class URLListItem
+    class URLListItem : INotifyPropertyChanged
     {
         public DateTime FirstOccurence { get; private set; }
 
         public string URL { get; private set; }
 
-        public int TotalCount { get; set; }
+        private int _TotalCount;
+        public int TotalCount
+        {
+            get { return _TotalCount; }
+            set
+            {
+                _TotalCount = value;
+                OnPropertyChanged("TotalCount");
+            }
+        }
 
         public HashSet<string> SentBy { get; private set; }
 
@@ -20,6 +30,17 @@ namespace TwitchURLGrabber
             FirstOccurence = firstSeen;
             URL = url;
             SentBy = new HashSet<string>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
